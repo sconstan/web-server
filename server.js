dda642d06d43c46f768655a4974932e53d839978
Savvas Konstantinidis <sconstan@gmail.com>
2016-02-07 19:50:08 +0200

    Updated language

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ var middleware = {
 app.use(middleware.logger);
 
 app.get('/about', middleware.requireAuthentication ,function(req, res) {
-	res.send('This is all about us!');
+	res.send('This is all about us!!');
 });
 
 app.use(express.static(__dirname+'/public'));
