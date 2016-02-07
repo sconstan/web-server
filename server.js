0376927195deb20504e3d880a3e06e557f895e67
Savvas Konstantinidis <sconstan@gmail.com>
2016-02-07 21:07:40 +0200

    Updated language

--- server.js
+++ server.js
@@ -2,17 +2,7 @@ var express = require('express');
 var app = express();
 const PORT = 3000;
 
-var middleware = {
-	requireAuthentication: function(req, res, next) {
-		console.log('private route hit!');
-		next();
-	},
-	logger: function(req, res, next) {
-		var reqDate = new Date().toString();
-		console.log('Request: ' + reqDate + ' ' + req.method + ' ' + req.originalUrl);
-		next();
-	}
-};
+var middleware = require ('./middleware.js');
 
 // app.get('/', function(req, res) {
 // 	res.send('Hello Express!');
